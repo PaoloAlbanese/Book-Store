ca85fcc858daf95606e7f8d628b590551e198fd8
Paolo Albanese <paulitus_@hotmail.com>
2021-06-08 20:00:53 +0100

    Testing accordion

--- js/script.js
+++ js/script.js
@@ -13,6 +13,14 @@ $(document).ready(function(){
       }
     });
     
+    $('.writers-accordion').click(function(event){
+      if(event.target.id.split('-')[0]==='button'){
+        $('#book-1').attr('src','ímg/writers/'+event.target.id.split('-')[1]+'-book1.jpg' );
+//         $('#book-1').attr('src','ímg/writers/William_Shakespeare-book1.jpg' );
+        $('#book-2').attr('src','ímg/writers/'+event.target.id.split('-')[1]+'-book2.jpg' );
+      }
+    });
+    
   })
 
 
