8ff91ab8ee514bad87e2fa3b7f44740120b57e91
Paolo Albanese <paulitus_@hotmail.com>
2021-06-09 11:11:49 +0100

    Added suggested books section

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
     $('.writers-accordion').click(function(event){
         
       if(event.target.id.split('-')[0]==='button'){
-          console.log('c\'e\' button',event.target.id.split('-')[0])
+          console.log('c\'e\' button',event.target.id.split('-')[0]);
         $('#book-1').attr('src','img/writers/'+event.target.id.split('-')[1]+'-book1.jpg' );
         $('#book-2').attr('src','img/writers/'+event.target.id.split('-')[1]+'-book2.jpg' );
       }
