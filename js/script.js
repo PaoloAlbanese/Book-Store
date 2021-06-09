e4a1a2b6b460fd5c568e17ffbb0579b03e8e816d
Paolo Albanese <55405144+PaoloAlbanese@users.noreply.github.com>
2021-06-09 07:54:16 +0200

    Update script.js
    corrected typo in script.js

--- js/script.js
+++ js/script.js
@@ -14,10 +14,11 @@ $(document).ready(function(){
     });
     
     $('.writers-accordion').click(function(event){
+        
       if(event.target.id.split('-')[0]==='button'){
-        $('#book-1').attr('src','ímg/writers/'+event.target.id.split('-')[1]+'-book1.jpg' );
-//         $('#book-1').attr('src','ímg/writers/William_Shakespeare-book1.jpg' );
-        $('#book-2').attr('src','ímg/writers/'+event.target.id.split('-')[1]+'-book2.jpg' );
+          console.log('c\'e\' button',event.target.id.split('-')[0])
+        $('#book-1').attr('src','img/writers/'+event.target.id.split('-')[1]+'-book1.jpg' );
+        $('#book-2').attr('src','img/writers/'+event.target.id.split('-')[1]+'-book2.jpg' );
       }
     });
     
